08f889515cb4f9b7bfa561ac47e7d0a505dfb0c5
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2017-12-14 17:43:54 +0000

    Bad naming conventions. Fixed now.

--- Ludenberg/src/Years.java
+++ Ludenberg/src/Years.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
-	
+public class Years {
+
 	public static boolean IsLeapYear(int year) {
 		// If a year is divisible by 400, it is a leap year.
 		if (year % 400 == 0) {
@@ -26,27 +26,27 @@ public class Main {
 		int notLeapYear = 1993;
 		int notLeapYear2 = 2003;
 		int notLeapYear3 = 2100;
-		
+
 		// Initialise a list to store the years
-		List<Integer> list = new ArrayList<>();
+		List<Integer> years = new ArrayList<>();
 		// Add the years to the new list
-		list.add(leapYear);
-		list.add(leapYear2);
-		list.add(notLeapYear);
-		list.add(notLeapYear2);
-		list.add(notLeapYear3);
-		
+		years.add(leapYear);
+		years.add(leapYear2);
+		years.add(notLeapYear);
+		years.add(notLeapYear2);
+		years.add(notLeapYear3);
+
 		// For each item in the years list,
-		for (int i = 0; i < list.size(); i++) {
+		for (int i = 0; i < years.size(); i++) {
 			// If it is a leap year,
-			if (IsLeapYear(list.get(i)) == true) {
+			if (IsLeapYear(years.get(i)) == true) {
 				// Print it out as so.
-				System.out.println(list.get(i) + " is a leap year.");
+				System.out.println(years.get(i) + " is a leap year.");
 			}
 			// If the year isn't a leap year,
 			else {
 				// Print it out as so.
-				System.out.println(list.get(i) + " is not a leap year.");
+				System.out.println(years.get(i) + " is not a leap year.");
 			}
 		}
 	}
