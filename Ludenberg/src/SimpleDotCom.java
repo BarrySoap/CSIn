43188c51e550b6d849ef88101e3825af74d30928
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2017-12-25 18:51:59 +0000

    Battleships game now has guessing functionality within one row.

--- Ludenberg/src/SimpleDotCom.java
+++ Ludenberg/src/SimpleDotCom.java
@@ -4,24 +4,30 @@ public class SimpleDotCom {
 	private int[] locationCells;
 	private int numOfHits = 0;
 	
-	public String checkGuess(String guess) {
-		int newGuess = Integer.parseInt(guess);
-		for (int i = 0; i < locationCells.length; i++) {
-			if (newGuess == locationCells[i]) {
-				numOfHits++;
-				if (numOfHits == 3) {
-					return "kill";
-				} else {
-					return "hit";
-				}
-			} else {
-				return "miss";
-			}
-		}
-		return guess;
+	public void setLocationCells(int[] loc) 
+	{
+		locationCells = loc;
 	}
 	
-	public void setLocationCells(int[] loc) {
-		locationCells = loc;
+	public String checkGuess(String guess) 
+	{
+		int newGuess = Integer.parseInt(guess);
+		String result = "miss";
+		for (int cell : locationCells) 
+		{
+			if (newGuess == cell) 
+			{
+				result = "hit";
+				numOfHits++;
+				break;
+			}
+		}
+		
+		if (numOfHits == locationCells.length) 
+		{
+			result = "kill";
+		}
+		System.out.println(result);
+		return result;
 	}
 }
--- Ludenberg/src/SimpleDotComGame.java
+++ Ludenberg/src/SimpleDotComGame.java
@@ -3,6 +3,26 @@ public class SimpleDotComGame {
 
 	public static void main(String[] args) {
 		
+		int numOfGuesses = 0;
+		SimpleDotCom simp = new SimpleDotCom();
+		GameHelper helper = new GameHelper();
+		int randStart = (int)(Math.random() * 5);
 		
+		int[] locations = {randStart, randStart + 1, randStart + 2};
+		simp.setLocationCells(locations);
+		boolean isAlive = true;
+		
+		while (isAlive == true) 
+		{
+			String guess = helper.getUserInput("Guess a number:");
+			String result = simp.checkGuess(guess);
+			numOfGuesses++;
+			
+			if (result.equals("kill")) 
+			{
+				isAlive = false;
+				System.out.println("You took " + numOfGuesses + " guesses.");
+			}
+		}
 	}
 }
