f694a60f29baa017d7e77fe57d0cec9cae2dcaa3
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-01-11 12:13:17 +0000

    Added a try/catch/finally block to the formatting class.

--- Ludenberg/src/Formatting.java
+++ Ludenberg/src/Formatting.java
@@ -3,12 +3,16 @@ import java.util.*;
 public class Formatting {
 
 	public static void main(String[] args) {
-		System.out.println("Today's date: " + String.format("%tc", new Date()));					// tc for the time & date
-		System.out.println("The time is: " + String.format("%tr", new Date()));						// tr for the time
-		System.out.println("The day of the week is: " + String.format("%tA", new Date()));			// tA for the day of the week
-		System.out.println("The month is: " + String.format("%tB", new Date()));					// tB for the month
-		System.out.println("The day of the month is: " + String.format("%td", new Date()));			// td for the day of the month
-		
-		System.out.println(String.format(" Decimals: %d \n Floating points: %.3f \n Hexadecimals: %x \n Characters: %c", 42, 42.000000, 42, 42));
+		try {
+			System.out.println("Today's date: " + String.format("%tc", new Date()));					// tc for the time & date
+			System.out.println("The time is: " + String.format("%tr", new Date()));						// tr for the time
+			System.out.println("The day of the week is: " + String.format("%tA", new Date()));			// tA for the day of the week
+			System.out.println("The month is: " + String.format("%tB", new Date()));					// tB for the month
+			System.out.println("The day of the month is: " + String.format("%td", new Date()));			// td for the day of the month
+		} catch (Exception e) {
+			System.out.println("Something failed.");
+		} finally {
+			System.out.println(String.format(" Decimals: %d \n Floating points: %.3f \n Hexadecimals: %x \n Characters: %c", 42, 42.000000, 42, 42));
+		}
 	}
 }
